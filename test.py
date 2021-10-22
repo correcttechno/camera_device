026dd06aa1e2a49f17cb45d89b193fc6807c0a74
Macbook <correcttechno@gmail.com>
2021-10-22 10:56:50 +0400

    changes

--- test.py
+++ test.py
@@ -4,24 +4,32 @@ import numpy as np
 import pytesseract
 import time
 from PIL import Image
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
 
 print("axtarilir")
 
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(640, 480))
+#camera = PiCamera()
+#camera.resolution = (640, 480)
+#camera.framerate = 30
+#rawCapture = PiRGBArray(camera, size=(640, 480))
+
+camera=cv2.VideoCapture(0)
+camera.set(3, 640)
+camera.set(4, 480)
+
+
 
 say=0
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+while True:
+        ret,rawCapture=camera.read()
         say=say+1
-        
-        image = frame.array
+            
+        image = rawCapture
         #cv2.imshow("Frame", image)
         key = cv2.waitKey(1) & 0xFF
-        rawCapture.truncate(0)
+        #rawCapture.truncate(0)
         if key != ord("s") and say==20:
              say=0
              gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert to grey scale
@@ -50,15 +58,15 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                  (topx, topy) = (np.min(x), np.min(y))
                  (bottomx, bottomy) = (np.max(x), np.max(y))
                  Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-                 #for i in range(3,14):
-                 text = pytesseract.image_to_string(Cropped, config='--psm 11 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-                 if(len(text)>=7):
-                     print("Detected Number is:",text)
-                     cv2.imshow("Cropped", Cropped)
+                 for i in range(3,14):
+                  text = pytesseract.image_to_string(Cropped, config='--psm 11 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+                  if(len(text)>=7):
+                      print("Detected Number is:",text)
+                      cv2.imshow("Cropped", Cropped)
              
              #server.sendmail("Sender's Email ID@gmail.com","Sender's Email ID@gmail.com",text)
              
-             cv2.imshow("Frame",edged );
+             cv2.imshow("Frame",edged )
              #cv2.waitKey(0)
              #break
 cv2.destroyAllWindows()
